75f030ca27d2885d550fc4404bc8f478d8f4808c
sakibhossen202 <67454665+sakibhossen202@users.noreply.github.com>
2020-08-18 23:56:43 +0600

    Done

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 
 const apiURL = "https://api.lyrics.ovh"
-const searchValue = document.querySelector("#searchValue")
-
-document.querySelector("#searchBtn").addEventListener("click", function () {
+function searchBtnHandler() {
 
     searchSong(searchValue.value);
+    console.log("search button clicked")
 
-
-
-
-
-})
+}
 
 
 //fetching data from server
@@ -25,8 +20,10 @@ function searchSong(searchKeys) {
             //displaying data in title & artist 
 
             function displayTitle(dataTaker) {
-                const lyricList = document.getElementById("lyricsList")
-                    for (let i = 0; i < 10; i++) {
+                let lyricList = document.getElementById("lyricsList")
+                lyricList.innerHTML = ''
+
+                for (let i = 0; i < 10; i++) {
                     const element = dataTaker[i];
                     const title = element.title;
                     const artist = element.artist.name
@@ -39,6 +36,7 @@ function searchSong(searchKeys) {
                         <div class="col-md-5 ">
                             <h3 class="lyrics-name" id="songTitle">${title}</h3>
                             <p class="author lead">Album by <span id="songArtist">${artist}</span></p>
+                           
                             
                         </div>
 
@@ -47,7 +45,7 @@ function searchSong(searchKeys) {
                         </div>
                         
                         <div class="col-md-2 text-md-center text-center">
-                            <button class="btn btn-success getLyrics" onclick="getLyricsBtnHandler(this)">Get Lyrics</button>
+                        <button class="btn btn-success getLyrics "onclick="getLyricsBtnHandler('${artist}','${title}')" >Get Lyrics</button>
                         </div>
                     </div>`
 
@@ -68,69 +66,56 @@ function searchSong(searchKeys) {
 
 //getLyrics part
 
-function getLyricsBtnHandler(lyrics) {
+function getLyricsBtnHandler(artist, title) {
     const getLyrics = document.getElementsByClassName("getLyrics")
     for (let i = 0; i < getLyrics.length; i++) {
-        
-        if(lyrics == getLyrics[i])
-        
-        {
-                const songArtist = document.getElementById("songArtist").innerHTML
-                const songTitle = document.getElementById("songTitle").innerHTML
-                fetch(`https://api.lyrics.ovh/v1/${songArtist}/${songTitle}`)
-                
-                .then(res => res.json())
-                .then(function displayTitle (lyrics){
-                    if(lyrics == undefined){
-                        document.getElementById("getLyricsDisplay").innerText = "Lyrics not Found"
-                    }
-                    const presentLyrics = lyrics.lyrics;
-                    console.log(presentLyrics)
-                    const lyricsDisplay = document.getElementById("getLyricsDisplay")
-                    lyricsDisplay.innerHTML = `<button class="btn go-back" onclick ="removeLyric()">Go Back</button>
-                    <h2 class="text-success mb-4">${songArtist}</h2>
-                    <pre class="lyric text-white">${presentLyrics}</pre>`
+        const element = getLyrics[i];
 
-                }
+        
+            const songArtist = artist;
+            const songTitle = title;
+            fetch(`https://api.lyrics.ovh/v1/${songArtist}/${songTitle}`)
+
+                .then(res => res.json())
+                .then(function lyricsUpdate(data){
+                   const lyricsName = data.lyrics
+                     let getLyricsDisplay = document.getElementById("getLyricsDisplay");
+
+                    getLyricsDisplay.innerHTML = `<button class="btn go-back" onclick="removeLyric()" >Go Back</button>
+                         <h2 class="text-success mb-4">${songTitle}</h2>
+                         <pre class="lyric text-white">${lyricsName}</pre>`
+
+
+                     console.log(lyricsName)
+
+                } 
+
+                )
+                     
+            }
+
+            function removeLyric() {
+    
+    
+            const lyricsDisplay = document.getElementById("getLyricsDisplay")
+            lyricsDisplay.remove()
+    
+    
+    
+    
+    }
+        }
                     
 
 
-                )
-        }
-    }
+
+                
+
+
+
         
-        
 
-    
 
-}
-
-function removeLyric(){
-    const lyricsDisplay = document.getElementById("getLyricsDisplay")
-    lyricsDisplay.remove()
-}
-
-function fancyList (musicName){
-    searchSong(musicName)
-    lyricList.innerHTML += `<div class="search-result   mx-auto py-4 row ">
-                    <!-- single result -->
-                    <div class="single-result row align-items-center my-3 p-3 text-center d-flex justify-content-between">
-                        <div class="col-md-5 ">
-                            <h3 class="lyrics-name" id="songTitle">${title}</h3>
-                            <p class="author lead">Album by <span id="songArtist">${artist}</span></p>
-                            
-                        </div>
-
-                        <div class="col-md-3 ">
-                            <img src ="${albumImage}">
-                        </div>
-                        
-                        <div class="col-md-2 text-md-center text-center">
-                            <button class="btn btn-success getLyrics" onclick="getLyricsBtnHandler(this)">Get Lyrics</button>
-                        </div>
-                    </div>`
-
-}
 
 
 
